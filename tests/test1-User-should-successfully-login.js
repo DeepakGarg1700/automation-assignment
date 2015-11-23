267c9bf0f088d2a92761b42a4dd4c0d184b908c8
DeepakGarg1700 <deepakgarg1700@gmail.com>
2015-11-24 02:09:14 +0530

    Fix #2

--- tests/test1-User-should-successfully-login.js
+++ tests/test1-User-should-successfully-login.js
@@ -5,7 +5,7 @@ module.exports = {
   'login to rtmedia demo' : function (client) {
     client
       .maximizeWindow()  // to maximize the window
-      .login('http://demo.rtcamp.com/rtmedia','demo'.'demo') // login 
+      .login('http://demo.rtcamp.com/rtmedia','demo','demo') // login 
       .assert.visible('.logout') // logout will be present if logged in successfully
       .assert.visible('#ab-pending-notifications') // Successfully pending notification is present
 
